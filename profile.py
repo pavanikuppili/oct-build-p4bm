737125249ffa3633ced5b95cfa08d07494f77770
Pavani Kuppili <41535888+pavanikuppili@users.noreply.github.com>
2026-01-30 22:47:40 -0700

    Update disk image to Ubuntu 22.04

--- profile.py
+++ profile.py
@@ -37,7 +37,7 @@ params = pc.bindParameters()
 node = request.XenVM('fpga-tools',exclusive=False)
 node.component_manager_id = "urn:publicid:IDN+cloudlab.umass.edu+authority+cm"
 #node.disk_image = params.diskImage
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU20-64-STD" 
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD" 
 node.setFailureAction('nonfatal')
 node.Desire("FPGA-Build-Pool", 1.0)
 
